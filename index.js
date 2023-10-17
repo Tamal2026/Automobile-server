7f696be30380888b252ab2846ce78b6a64564886
Tamal1 <tamalkrishna2026@gmail.com>
2023-10-18 02:49:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('website is running')
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is Running on port:${port}`);
+})
