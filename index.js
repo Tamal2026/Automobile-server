03597bd190a20acf36c9a6a3c2c3345572c36a7c
Tamal1 <tamalkrishna2026@gmail.com>
2023-10-18 16:37:33 +0600

    Dynamically Product Added to database and Send response to the clint side

--- index.js
+++ index.js
@@ -10,6 +10,47 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://AutoMobile:GBq4yUltnjzjik6Y@try-myself.0cjln25.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('ProductDB').collection('product')
+
+app.get('/product',async(req,res)=>{
+    const cursor = productCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+
+})
+
+    app.post('/product',async(req,res)=>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
